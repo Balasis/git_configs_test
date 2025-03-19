49eea809c9397fc817537d734c44e859b3c9a510
Balasis_Ioannis <giovani1994a@gmail.com>
2025-03-19 23:40:15 +0200

    few changes to be tested

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //JSON parsing middleware
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { message: "Page not found" });
+});
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
